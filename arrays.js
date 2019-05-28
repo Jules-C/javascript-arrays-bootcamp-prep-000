b1429311cceefc1cf9c838d9b4a4aa9404d59cce
Julie Cook <juliecook328@gmail.com>
2019-05-28 01:04:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,5 @@ function addElementToBeginningOfArray(array, element) {
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return destructivelyAddElementToBeginningOfArray.unshift("element");
+  return destructivelyAddElementToBeginningOfArray.unshift(element);
 }
