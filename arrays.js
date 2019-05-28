b5849c93e3bf04ed9ad238d384c471174917bd44
Julie Cook <juliecook328@gmail.com>
2019-05-28 01:56:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,4 +23,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
-function 
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+  return array
+}
